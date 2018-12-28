81e497dec0965ab365a58ae11e37cc387327620d
niraj <ctoulaniraj@gmail.com>
2018-12-28 14:02:58 +0545

    changes done

--- models/tododb.js
+++ models/tododb.js
@@ -13,6 +13,15 @@ const todo =[
             '1': 'Sunrise',
             '2': 'Hiking upto Nagarkot view tower',
         }
+    },
+    {
+        destination:'shivapuri',
+        actionsToDo:{
+            '1':'animal hunt',
+            '2':'nagi gumbha',
+            '3':'hiking till shivapuri peak',
+            '4':'meditation'
+        }
     }
 ]
 module.exports = todo;
